5ce474e11c694803722d5397dc760e78fee9b94a
Matt Carter <m@ttcarter.com>
2016-09-22 16:26:05 +1000

    Added RCT filter

--- test/templates.js
+++ test/templates.js
@@ -31,10 +31,10 @@ describe('Translate the meta template `<engine>`', function() {
 });
 
 
-describe.skip('Translate the meta template `<rct filter>`', function() {
+describe('Translate the meta template `<rct filter>`', function() {
 
 	it('translate `<RCT Filter>` -> PM `pubmed`', function() {
-		expect(polyglot.translate('<RCT Filter>', 'pubmed')).to.equal('');
+		expect(polyglot.translate('<RCT Filter>', 'pubmed')).to.equal('randomized controlled trial[pt] OR controlled clinical trial[pt] OR randomized[tiab] OR randomised[tiab] OR placebo[tiab] OR "drug therapy"[MeSH] OR randomly[tiab] OR trial[tiab] OR groups[tiab] NOT (Animals[Mesh] not (Animals[Mesh] and Humans[Mesh]))');
 	});
 
 	it('translate PM `<RCT Filter>` -> OV `ovid`', function() {
@@ -46,15 +46,15 @@ describe.skip('Translate the meta template `<rct filter>`', function() {
 	});
 
 	it('translate PM `<RCT Filter>` -> EM `embase`', function() {
-		expect(polyglot.translate('<RCT Filter>', 'embase')).to.equal('');
+		expect(polyglot.translate('<RCT Filter>', 'embase')).to.equal("random* OR factorial OR crossover OR placebo OR blind OR blinded OR assign OR assigned OR allocate OR allocated OR 'crossover procedure'/exp OR 'double-blind procedure'/exp OR 'randomized controlled trial'/exp OR 'single-blind procedure'/exp NOT ('animal'/exp NOT ('animal'/exp AND 'human'/exp))");
 	});
 
 	it('translate PM `<RCT Filter>` -> CI `cinahl`', function() {
-		expect(polyglot.translate('<RCT Filter>', 'cinahl')).to.equal('');
+		expect(polyglot.translate('<RCT Filter>', 'cinahl')).to.equal('(MH "Clinical Trials+") OR (MH "Quantitative Studies") OR TI placebo* OR AB placebo* OR (MH "Placebos") OR (MH "Random Assignment") OR TI random* OR AB random* OR TI ((singl* or doubl* or tripl* or trebl*) W1 (blind* or mask*)) OR AB ((singl* or doubl* or tripl* or trebl*) W1 (blind* or mask*)) OR TI clinic* trial* OR AB clinic* trial* OR PT clinical trial');
 	});
 
 	it('translate PM `<RCT Filter>` -> WS `wos`', function() {
-		expect(polyglot.translate('<RCT Filter>', 'wos')).to.equal('');
+		expect(polyglot.translate('<RCT Filter>', 'wos')).to.equal('TS=(random* or placebo* or allocat* or crossover* or "cross over" or ((singl* or doubl*) NEAR/1 blind*)) OR TI=(trial)');
 	});
 
 });
