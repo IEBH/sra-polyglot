f2f0b06f45b4e1a8f636c2495f407c7be3ce8c8e
Matt Carter <m@ttcarter.com>
2017-07-05 16:52:19 +1000

    REFACTOR: More test cleanups

--- test/fieldGroups.js
+++ test/fieldGroups.js
@@ -3,15 +3,15 @@ var polyglot = require('..');
 
 describe('Translate field groups (Ovid -> CINAHL)', ()=> {
 
-	it('translate OV `(term1).tw.` -> CI `(TI term1 OR AB term1)`', ()=> {
+	it('translate `(term1).tw.` -> CI `(TI term1 OR AB term1)`', ()=> {
 		expect(polyglot.translate('(term1).tw.', 'cinahl')).to.equal('(TI term1 OR AB term1)');
 	});
 
-	it('translate OV `(term1 OR term2).tw.` -> CI `(TI term1 OR AB term1 OR TI term2 OR AB term2)`', ()=> {
+	it('translate `(term1 OR term2).tw.` -> CI `(TI term1 OR AB term1 OR TI term2 OR AB term2)`', ()=> {
 		expect(polyglot.translate('(term1 OR term2).tw.', 'cinahl')).to.equal('(TI term1 OR AB term1 OR TI term2 OR AB term2)');
 	});
 
-	it('translate OV `((term1 OR term2) AND term3).tw.` -> CI `(TI term1 OR AB term1 OR TI term2 OR AB term2)`', ()=> {
+	it('translate `((term1 OR term2) AND term3).tw.` -> CI `(TI term1 OR AB term1 OR TI term2 OR AB term2)`', ()=> {
 		expect(polyglot.translate('((term1 OR term2) AND term3).tw.', 'cinahl')).to.equal('((TI term1 OR AB term1 OR TI term2 OR AB term2) AND TI term3 OR AB term3)');
 	});
 
--- test/mangling.js
+++ test/mangling.js
@@ -3,7 +3,7 @@ var polyglot = require('..');
 
 describe('Translate search phrases (with mangled quote marks)', ()=> {
 
-	it('translate PM `“term1 term2”` -> PM `"term1 term2"`', ()=> {
+	it('translate `“term1 term2”` -> PM `"term1 term2"`', ()=> {
 		expect(polyglot.translate('“term1 term2”', 'pubmed')).to.equal('"term1 term2"');
 	});
 
