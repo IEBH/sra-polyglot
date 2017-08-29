e458a39ae29bfab0ed432a8d88431bd74967a59e
Matt Carter <m@ttcarter.com>
2017-08-29 14:15:57 +1000

    Added new line-scanning mode which fixes issues with line numberings without screwing up proximity detection

--- test/numbering.js
+++ test/numbering.js
@@ -3,8 +3,11 @@ var polyglot = require('..');
 
 describe('Translate searches and ignore numbering', ()=> {
 
-	it('translate multi-line with numbering', ()=> {
+	it('translate multi-line with numbering (Number + Dot + Space)', ()=> {
 		expect(polyglot.translate('1. Foo\nAND\n2. Bar\nAND\n3. Baz', 'pubmed', {html: false})).to.equal('(Foo)\nAND\n(Bar)\nAND\n(Baz)');
 	});
 
+	it('translate multi-line with numbering (Number + Space)', ()=> {
+		expect(polyglot.translate('1 Foo\nAND\n2 Bar\nAND\n3 Baz', 'pubmed', {html: false})).to.equal('(Foo)\nAND\n(Bar)\nAND\n(Baz)');
+	});
 });
