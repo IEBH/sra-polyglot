36747d9ee8de3db52978d6ae2b1dd17cbcbf4645
Matt Carter <m@ttcarter.com>
2018-01-17 14:56:30 +1000

    BUGFIX: Change to line output test

--- test/lines.js
+++ test/lines.js
@@ -25,31 +25,95 @@ describe.skip('Line numbers', ()=> {
 		expect(polyglot.translate(input, 'wos')).to.equal(output);
 	});
 
-	it('should expand collapsed conditionals', ()=> {
-		var input =
+})
+
+// FIXME: Not currently supported
+describe.skip('Line expression expansion', ()=> {
+	var input =
+		'1 Foo AND\n' +
+		'2 Bar AND\n' +
+		'3 Baz OR\n' +
+		'4 Quz\n' +
+		'5 OR /1-4';
+
+	it('translate line expansion format -> PM', ()=> {
+		expect(polyglot.translate('term*', 'pubmed')).to.equal(
 			'1 Foo AND\n' +
 			'2 Bar AND\n' +
 			'3 Baz OR\n' +
 			'4 Quz\n' +
-			'5 OR /1-4';
+			'5 #1 OR #2 OR #3 OR #4'
+		);
+	});
 
-		var output =
+	it('translate line expansion format -> OV', ()=> {
+		expect(polyglot.translate('term*', 'ovid')).to.equal(
 			'1 Foo AND\n' +
 			'2 Bar AND\n' +
 			'3 Baz OR\n' +
 			'4 Quz\n' +
-			'5 #1 OR #2 OR #3 OR #4';
+			'5 1 OR 2 OR 3 OR 4'
+		);
+	});
 
+	it('translate line expansion format -> CO', ()=> {
+		expect(polyglot.translate('term*', 'cochrane')).to.equal(
+			'1 Foo AND\n' +
+			'2 Bar AND\n' +
+			'3 Baz OR\n' +
+			'4 Quz\n' +
+			'5 #1 OR #2 OR #3 OR #4'
+		);
+	});
 
-		// All engines should act the same so don't bother to break them up into different tests
-		expect(polyglot.translate(input, 'pubmed')).to.equal(output);
-		expect(polyglot.translate(input, 'ovid')).to.equal(output);
-		expect(polyglot.translate(input, 'cochrane')).to.equal(output);
-		expect(polyglot.translate(input, 'embase')).to.equal(output);
-		expect(polyglot.translate(input, 'cinahl')).to.equal(output);
-		expect(polyglot.translate(input, 'psycinfo')).to.equal(output);
-		expect(polyglot.translate(input, 'scopus')).to.equal(output);
-		expect(polyglot.translate(input, 'wos')).to.equal(output);
+	it('translate line expansion format -> EM', ()=> {
+		expect(polyglot.translate('term*', 'embase')).to.equal(
+			'1 Foo AND\n' +
+			'2 Bar AND\n' +
+			'3 Baz OR\n' +
+			'4 Quz\n' +
+			'5 #1 OR #2 OR #3 OR #4'
+		);
+	});
+
+	it('translate line expansion format -> CI', ()=> {
+		expect(polyglot.translate('term*', 'cinahl')).to.equal(
+			'1 Foo AND\n' +
+			'2 Bar AND\n' +
+			'3 Baz OR\n' +
+			'4 Quz\n' +
+			'5 S1 OR S2 OR S3 OR S4'
+		);
+	});
+
+	it('translate line expansion format -> PY', ()=> {
+		expect(polyglot.translate('term*', 'psycinfo')).to.equal(
+			'1 Foo AND\n' +
+			'2 Bar AND\n' +
+			'3 Baz OR\n' +
+			'4 Quz\n' +
+			'5 #1 OR #2 OR #3 OR #4'
+		);
+	});
+
+	it('translate line expansion format -> SC', ()=> {
+		expect(polyglot.translate('term*', 'scopus')).to.equal(
+			'1 Foo AND\n' +
+			'2 Bar AND\n' +
+			'3 Baz OR\n' +
+			'4 Quz\n' +
+			'5 #1 OR #2 OR #3 OR #4'
+		);
+	});
+
+	it('translate line expansion format -> WS', ()=> {
+		expect(polyglot.translate('term*', 'wos')).to.equal(
+			'1 Foo AND\n' +
+			'2 Bar AND\n' +
+			'3 Baz OR\n' +
+			'4 Quz\n' +
+			'5 #1 OR #2 OR #3 OR #4'
+		);
 	});
 
 });
