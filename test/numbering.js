c4b5f75894cf6e535097212ca7fbc5e48dbacf20
Matt Carter <m@ttcarter.com>
2018-11-01 10:43:41 +1000

    First test writeup for line number replacement (not yet working)

--- test/numbering.js
+++ test/numbering.js
@@ -11,3 +11,19 @@ describe('Translate searches and ignore numbering', ()=> {
 		expect(polyglot.translate('1 Foo\nAND\n2 Bar\nAND\n3 Baz', 'pubmed', {html: false})).to.equal('(Foo)\nAND\n(Bar)\nAND\n(Baz)');
 	});
 });
+
+describe.skip('Allow reference numbering', ()=> {
+
+	it('should be able to reference lines', ()=> {
+		var inputPrefix = 'Foo\nAND\nBar\nAND\nBaz\n';
+		var inputPrefixResult = '(Foo)\nAND\n(Bar)\nAND\n(Baz)';
+
+		expect(polyglot.translate(inputPrefix, 'pubmed', {html: false})).to.equal(inputPrefixResult);
+		expect(polyglot.translate(inputPrefix + 'AND\n1', 'pubmed', {html: false})).to.equal(inputPrefixResult + '\nAND\n(Foo)');
+		expect(polyglot.translate(inputPrefix + 'AND\n1 OR 2 OR 3', 'pubmed', {html: false})).to.equal(inputPrefixResult + '\nAND\n(Foo) OR (Bar) OR (Baz)');
+		expect(polyglot.translate(inputPrefix + 'AND\n1 - 3/OR', 'pubmed', {html: false})).to.equal(inputPrefixResult + '\nAND\n(Foo) OR (Bar) OR (Baz)');
+		expect(polyglot.translate(inputPrefix + 'AND\n1 - 3/AND', 'pubmed', {html: false})).to.equal(inputPrefixResult + '\nAND\n(Foo) AND (Bar) AND (Baz)');
+
+	})
+
+});
