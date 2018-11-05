eab5dda8fc51536f57e75b6820a0bdbfcca94743
Matt Carter <m@ttcarter.com>
2018-11-06 09:07:28 +1000

    Vue based demo now fully working (minus ACE editor)

--- demo/app.js
+++ demo/app.js
@@ -1,10 +1,45 @@
 // var Vue = require('vue');
+import _ from 'lodash';
 import Vue from 'vue/dist/vue.esm.js';
-var app;
+import polyglot from 'polyglot';
 
-$(()=> app = new Vue({
+$(()=> window.app = new Vue({
 	el: '#app',
 	data: {
-		message: 'Hello World!',
+		query: '',
+		engines: polyglot.engines,
+		enginesExpanded: {},
+		enginesQuery: {},
+		polyglotOptions: {
+			groupLines: false,
+			groupLinesAlways: true,
+			removeNumbering: true,
+			preserveNewLines: true,
+			replaceWildcards: true,
+			transposeLines: true,
+		},
+		exampleLast: '',
+	},
+	methods: {
+		clear() {
+			this.query = '';
+		},
+		showExample() {
+			var chosenExample;
+			do {
+				chosenExample = _.sample(polyglot.examples);
+			} while (this.exampleLast == chosenExample.title)
+			this.exampleLast = chosenExample;
+			this.query = chosenExample.query;
+		},
+		toggleExpandEngine(engine) {
+			this.$set(this.enginesExpanded, engine.id, !this.enginesExpanded[engine.id]);
+		},
+	},
+	watch: {
+		query() {
+			_(polyglot.translateAll(this.query, this.polyglotOptions))
+				.forEach((query, key) => this.$set(this.enginesQuery, key, query))
+		},
 	},
 }));
