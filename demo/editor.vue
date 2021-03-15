b5fab02e9283998efb67c2b76aaca5caf57f8f23
Connor Forbes <connorforbes25@gmail.com>
2021-03-15 14:08:43 +1000

    style: Added navbar with cite and help buttons

--- demo/editor.vue
+++ demo/editor.vue
@@ -155,98 +155,94 @@ export default {
 </script>
 
 <template>
-	<div class="container">
-		<div v-on:click="openLink('https://www.ncbi.nlm.nih.gov/pubmed/32256231')" class="alert alert-info text-center">
-			<div class="pull-left font-xl h1">
-				<i class="fa fa-question-circle push-up"></i>
+	<div>
+		<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+			<a class="navbar-brand" href="#">Polyglot</a>
+			<div class="ml-auto">
+				<button class="btn btn-success" @click="openLink('https://www.ncbi.nlm.nih.gov/pubmed/32256231')">Cite</button>
+				<button class="btn btn-info ml-2" @click="openLink('http://sr-accelerator.com/#/help/polyglot')">Help</button>
 			</div>
-			Click here to cite the Polyglot tool
-		</div>
-		<div v-on:click="openLink('http://sr-accelerator.com/#/help/polyglot')" class="alert alert-info text-center">
-			<div class="pull-left font-xl h1">
-				<i class="fa fa-question-circle push-up"></i>
-			</div>
-			Click here to access the Polyglot help guide 
-		</div>
-
-		<div class="row-fluid">
-			<div class="card">
-				<div class="card-header">
-					Your query
-					<div class="pull-right">
-						<input type="checkbox" id="checkbox" v-model="polyglotOptions.transposeLines">
-						<label for="checkbox">Replace Line References</label>
-						<a v-on:click="clear()" class="btn btn-sm btn-default"><i class="fa fa-eraser" title="Clear search"></i></a>
-						<a v-on:click="copyQuery()" class="btn btn-sm btn-default"><i class="fa fa-clipboard" title="Copy to clipboard"></i></a>
-						<a v-on:click="showExample()" class="btn btn-sm btn-default"><i class="fa fa-random" title="Show a random example"></i></a>
-						<span class="dropdown">
-							<a class="btn btn-sm btn-default" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-								<i class="fa fa-caret-down" title="Insert Template"></i>
-							</a>
-							<div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-								<a v-for="(template, key) in global.templates" :key="key" class="dropdown-item" href="#" v-on:click="insertTemplate(key)">{{template.name}}</a>
-							</div>
-						</span>
+		</nav>
+		<div class="container mt-3">
+			<div class="row-fluid">
+				<div class="card">
+					<div class="card-header">
+						Your query
+						<div class="pull-right">
+							<input type="checkbox" id="checkbox" v-model="polyglotOptions.transposeLines">
+							<label for="checkbox">Replace Line References</label>
+							<a v-on:click="clear()" class="btn btn-sm btn-default"><i class="fa fa-eraser" title="Clear search"></i></a>
+							<a v-on:click="copyQuery()" class="btn btn-sm btn-default"><i class="fa fa-clipboard" title="Copy to clipboard"></i></a>
+							<a v-on:click="showExample()" class="btn btn-sm btn-default"><i class="fa fa-random" title="Show a random example"></i></a>
+							<span class="dropdown">
+								<a class="btn btn-sm btn-default" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+									<i class="fa fa-caret-down" title="Insert Template"></i>
+								</a>
+								<div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+									<a v-for="(template, key) in global.templates" :key="key" class="dropdown-item" href="#" v-on:click="insertTemplate(key)">{{template.name}}</a>
+								</div>
+							</span>
+						</div>
+					</div>
+					<div class="card-body p-0">
+						<editor
+							ref='queryEditor'
+							v-model="query"
+							v-on:init="editorInit"
+							lang="polyglot"
+							theme="chrome"
+							width="100%"
+							height="380"
+							v-bind:options="editorOptions"
+						></editor>
+					</div>
+						<div v-if="!query" v-on:click="showExample()" class="card-footer text-center">
+						Type a PubMed or Ovid MEDLINE query in the box above to see its translations
+						<span class="text-muted">(or click here to see an example)</span>
 					</div>
 				</div>
-				<div class="card-body p-0">
-					<editor
-						ref='queryEditor'
-						v-model="query"
-						v-on:init="editorInit"
-						lang="polyglot"
-						theme="chrome"
-						width="100%"
-						height="380"
-						v-bind:options="editorOptions"
-					></editor>
-				</div>
-					<div v-if="!query" v-on:click="showExample()" class="card-footer text-center">
-					Type a PubMed or Ovid MEDLINE query in the box above to see its translations
-					<span class="text-muted">(or click here to see an example)</span>
-				</div>
 			</div>
-		</div>
 
-		<label class="text-reader">
-			<span class="select-button" @click="openSearchRefiner">Open Query in SearchRefiner</span>
-  		</label>
-		<label class="text-reader">
-			<span class="select-button">Import Search From .txt File</span>
-			<input type="file" @change="loadTextFromFile">
-  		</label>
-		
-		<hr/>
+			<label class="text-reader">
+				<span class="select-button" @click="openSearchRefiner">Open Query in SearchRefiner</span>
+			</label>
+			<label class="text-reader">
+				<span class="select-button">Import Search From .txt File</span>
+				<input type="file" @change="loadTextFromFile">
+			</label>
+			
+			<hr/>
 
-		<div class="accordion panel-group">
-			<div v-for="engine in engines" :key="engine.id" class="card" id="customcard">
-				<div class="card-header" v-on:click="toggleExpandEngine(engine)" >
-					<a class="accordion-toggle collapsed">
-						<i class="fa fa-fw" :class="enginesExpanded[engine.id] ? 'fa-chevron-down' : 'fa-chevron-right'"></i>
-						{{engine.title}}
-					</a>
-					<div class="pull-right">
-						<a v-if="engine.id != 'lexicalTreeJSON'" v-on:click.stop="copyContent(engine.id)" class="btn btn-sm btn-default"><i class="fa fa-clipboard" title="Copy to clipboard"></i></a>
+			<div class="accordion panel-group">
+				<div v-for="engine in engines" :key="engine.id" class="card" id="customcard">
+					<div class="card-header" v-on:click="toggleExpandEngine(engine)" >
+						<a class="accordion-toggle collapsed">
+							<i class="fa fa-fw" :class="enginesExpanded[engine.id] ? 'fa-chevron-down' : 'fa-chevron-right'"></i>
+							{{engine.title}}
+						</a>
+						<div class="pull-right">
+							<a v-if="engine.id != 'lexicalTreeJSON'" v-on:click.stop="copyContent(engine.id)" class="btn btn-sm btn-default"><i class="fa fa-clipboard" title="Copy to clipboard"></i></a>
+						</div>
+					</div>
+					<div class="card-body collapse" :class="enginesExpanded[engine.id] && 'show'">
+						<TemplateRender 
+							v-if="enginesQuery[engine.id] && engine.id != 'lexicalTreeJSON' && engine.id != 'mongodb'" 
+							:template="`<div>${enginesQuery[engine.id]}</div>`"
+							:query="query"
+							@replaceFields="query = $event"
+						/>
+						<jsontree v-if="enginesQuery[engine.id] && engine.id == 'lexicalTreeJSON'" :data="enginesQuery[engine.id]"></jsontree>
+						<hr>
+						<!-- MongoDB not included at this stage -->
 					</div>
 				</div>
-				<div class="card-body collapse" :class="enginesExpanded[engine.id] && 'show'">
-					<TemplateRender 
-						v-if="enginesQuery[engine.id] && engine.id != 'lexicalTreeJSON' && engine.id != 'mongodb'" 
-						:template="`<div>${enginesQuery[engine.id]}</div>`"
-						:query="query"
-						@replaceFields="query = $event"
-					/>
-					<jsontree v-if="enginesQuery[engine.id] && engine.id == 'lexicalTreeJSON'" :data="enginesQuery[engine.id]"></jsontree>
-      				<hr>
-					<!-- MongoDB not included at this stage -->
+			</div>
+			<div v-on:click="openLink('https://github.com/IEBH/sra-polyglot/blob/master/README.md')" class="alert alert-info text-center">
+				<div class="pull-left font-xl h1">
+					<i class="fa fa-question-circle push-up"></i>
 				</div>
+				Click here to open user reference guide for Polyglot
 			</div>
-		</div>
-		<div v-on:click="openLink('https://github.com/IEBH/sra-polyglot/blob/master/README.md')" class="alert alert-info text-center">
-			<div class="pull-left font-xl h1">
-				<i class="fa fa-question-circle push-up"></i>
-			</div>
-			Click here to open user reference guide for Polyglot
 		</div>
 	</div>
 </template>
