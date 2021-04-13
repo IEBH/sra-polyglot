382b2e8ce47018fe30d5f23f604b3ebf5f940743
Connor Forbes <connorforbes25@gmail.com>
2021-04-13 12:58:55 +1000

    refactor: modulated code

--- data/preprocess.js
+++ data/preprocess.js
@@ -1,4 +1,4 @@
-import xlsx from 'xlsx';
+import xlsxToParseMap from "./modules/xlsxToParseMap.js"
 
 /**
 * Testkit settings
@@ -14,55 +14,4 @@ var settings = {
 	dataRowStart: 1,
 };
 
-/**
-* Collection of row sources to use when running a test
-* This is calculated from the input data
-* @type {array<Object>} Collection of sources extracted from the input data
-* @property {string} id The row ID to use to extract data per line
-*/
-var sources;
-
-var xlsxToParseObject = () => {
-    Promise.resolve()
-        .then(()=> xlsx.readFile(`./polyglotV4.xlsx`))
-        .then(workbook => {
-            return xlsx.utils.sheet_to_json(workbook.Sheets[settings.sheet]);
-        })
-        // Extract source rows to aim for from the data set
-        .then(sheet => {
-            // Calculate sources
-            sources = Object.keys(sheet[0])
-                .filter(header =>
-                    ![...settings.omitCols, settings.rowHeader].includes(header) // Skip omitted columns
-                )
-                .map(header => ({
-                    id: header
-                }));
-            // Return sliced data - removing all header areas
-            return sheet.slice(settings.dataRowStart); // Remove
-        })
-        // Create lookup map 
-        .then(sheet => {
-            let lookupMap = new Map();
-            sheet.forEach((row, rowIndex) => {
-                sources.forEach(source => {
-                    if(row[source.id]) {
-                        var match = row[source.id].match(/Term(?<fieldCode>[^\s]*)/); // Only does basic match
-                        if (match.groups.fieldCode) {
-                            // TODO: Add logic if the field code could have different variations (e.g. .ti,ab,kf.)
-                            // Push fieldCode and explanation to Map
-                            lookupMap.set(match.groups.fieldCode, row[settings.rowHeader])
-                        } else {
-                            console.error(`\n${row[source.id]} failed to match field code\n`)
-                        }
-                    } else {
-                        console.error(`\n${source.id}'s "${row[settings.rowHeader]}" is undefined\n`)
-                    }
-                })
-            })
-            return lookupMap;
-        })
-        .then(lookupMap => console.log(lookupMap))
-}
-
-xlsxToParseObject();
+xlsxToParseMap(settings).then(lookupMap => console.log(lookupMap));
