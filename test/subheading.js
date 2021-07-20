76df41c5f7c56449674935bd1c8efc15b1a21ac2
Connor Forbes <connorforbes25@gmail.com>
2021-07-20 13:11:25 +1000

    test: More subheading tests

--- test/subheading.js
+++ test/subheading.js
@@ -14,6 +14,9 @@ describe('Translate subheadings with or without quotes (Ovid -> *)', ()=> {
 
 describe('Translate subheadings at end of mesh term (PubMed -> *)', ()=> {
 
+	it('translate `psoriasis/dt[Majr]` -> PM `psoriasis/dt[Majr]`', ()=> {
+		expect(polyglot.translate('psoriasis/dt[Majr]', 'PubMed abbreviation')).to.equal('psoriasis/dt[Majr]');
+	});
 	it('translate `psoriasis/dt[Majr]` -> OV `exp *psoriasis/dt`', ()=> {
 		expect(polyglot.translate('psoriasis/dt[Majr]', 'Ovid MEDLINE')).to.equal('exp *psoriasis/dt');
 	});
@@ -24,6 +27,9 @@ describe('Translate subheadings at end of mesh term (Ovid -> *)', ()=> {
 
 	it('translate `exp *psoriasis/dt` -> PM `psoriasis/dt[majr]`', ()=> {
 		expect(polyglot.translate('exp *psoriasis/dt', 'PubMed abbreviation')).to.equal('psoriasis/dt[Majr]');
+	});
+	it('translate `exp *psoriasis/dt` -> OV `exp *psoriasis/dt`', ()=> {
+		expect(polyglot.translate('exp *psoriasis/dt', 'Ovid MEDLINE')).to.equal('exp *psoriasis/dt');
 	});
 
 });
