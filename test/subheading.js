6dc20843115e6c001d458c717fa1c032805177e5
Connor Forbes <connorforbes25@gmail.com>
2021-07-20 10:25:08 +1000

    test: Updated test kit with new subheading case

--- test/subheading.js
+++ test/subheading.js
@@ -1,13 +1,21 @@
 var expect = require('chai').expect;
 const polyglot = require('../lib').default;
-describe('Translate substance searches (PubMed -> *)', ()=> {
+describe('Translate subheadings with or without quotes (Ovid -> *)', ()=> {
 
-	it('translate `"abnormalities".fs.` -> CH `term[nm]`', ()=> {
+	it('translate `"abnormalities".fs.` -> CH `[mh /AB]`', ()=> {
 		expect(polyglot.translate('"abnormalities".fs.', 'Cochrane Library')).to.equal('[mh /AB]');
 	});
 
-	it('translate `abnormalities.fs.` -> CH `term.nm`', ()=> {
+	it('translate `abnormalities.fs.` -> CH `[mh /AB]`', ()=> {
 		expect(polyglot.translate('abnormalities.fs.', 'Cochrane Library')).to.equal('[mh /AB]');
+	});
+
+});
+
+describe('Translate subheadings at end of mesh term (PubMed -> *)', ()=> {
+
+	it('translate `exp *psoriasis/dt` -> PM `psoriasis/dt[majr]`', ()=> {
+		expect(polyglot.translate('exp *psoriasis/dt', 'PubMed abbreviation')).to.equal('psoriasis/dt[majr]');
 	});
 
 });
