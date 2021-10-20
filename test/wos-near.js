b70e50f838b4c51395a5b4ee2fd32ef74c2cda44
Connor Forbes <connorforbes25@gmail.com>
2021-10-20 10:53:42 +1000

    test: Added another test case

--- test/wos-near.js
+++ test/wos-near.js
@@ -7,4 +7,8 @@ describe('Translate adj3 to WOS (Ovid -> *)', ()=> {
 		expect(polyglot.translate('(muscle$ adj3 resist$).ti,ab.', 'WoS Advanced')).to.equal('(TI=(muscle? NEAR/3 resist?) OR AB=(muscle? NEAR/3 resist?))');
 	});
 
+	it('translate `((muscle or muscles) adj3 (resist or resists)).ti,ab.` -> WoS `(TI=((muscle OR muscles) NEAR/3 (resist OR resists)) OR AB=((muscle OR muscles) NEAR/3 (resist OR resists)))`', ()=> {
+		expect(polyglot.translate('((muscle or muscles) adj3 (resist or resists)).ti,ab.', 'WoS Advanced')).to.equal('(TI=((muscle OR muscles) NEAR/3 (resist OR resists)) OR AB=((muscle OR muscles) NEAR/3 (resist OR resists)))');
+	});
+
 });
